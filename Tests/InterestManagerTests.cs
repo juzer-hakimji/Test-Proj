d69f7dd99cbb834b64de694b7c29cd5f40877d9d
juzerSF <juzerhakimji@gmail.com>
2022-12-20 13:24:47 +0530

    all test cases

--- Tests/InterestManagerTests.cs
+++ Tests/InterestManagerTests.cs
@@ -38,44 +38,78 @@ public class InterestManagerTests
 	}
 
 	[DataTestMethod]
-	[DataRow(1000, 10, 2, 0)]
-	[DataRow(1000, 20, 5, 0)]
-	public void check_calculateInterest_returns_correct_amount(int principal, double interestRate, int period, int expectedInterest)
+	[DataRow(15000, 10, 2, 3150.00)]
+	[DataRow(25000, 20, 5, 37208.00)]
+	public void check_calculateInterest_returns_correct_amount(int principal, double interestRate, int period, double expectedInterestAmount)
 	{
+		// Given I have Interest Data
+		var interestData = new Interest
+		{
+			principal = principal,
+			interestRate = interestRate,
+			period = period
+		};
 
-		Assert.AreEqual(1, 1);
+		// And I have mocked my interest repository to return interest data
+		var moqManager = new Mock<IInterestRepository>();
+		moqManager.Setup(m => m.GetInterestData(It.IsAny<Guid>())).Returns(interestData);
+
+		// And I have instantiated my manager
+		var interestManager = new InterestManager(moqManager.Object);
+
+		// When I call the service
+		var result = interestManager.CalculateInterest();
+
+		// Then I expect the resulting interest amount to match the expected interest amount
+		Assert.AreEqual(Math.Round(expectedInterestAmount, 2), Math.Round(result, 2));
 	}
 
-	// [DataTestMethod]
-	// [DynamicData(nameof(GetCalculateInterestData), DynamicDataSourceType.Method)]
-	// public void check_calculateInterest_returns_correct_amount2(int principal, double interestRate, int period, int expectedInterest)
-	// {
-	// 	Assert.AreEqual(1, 1);
-	// }
+	[DataTestMethod]
+	[DynamicData(nameof(GetCalculateInterestData), DynamicDataSourceType.Method)]
+	public void check_calculateInterest_returns_correct_amount2(Interest interestData, double expectedInterestAmount)
+	{
+		// I have mocked my interest repository to return interest data
+		var moqManager = new Mock<IInterestRepository>();
+		moqManager.Setup(m => m.GetInterestData(It.IsAny<Guid>())).Returns(interestData);
 
-	// private static IEnumerable<object[]> GetCalculateInterestData()
-	// {
-	// 	var today = DateTime.Today;
-	// 	var financeId = Guid.NewGuid();
-	// 	var finance = new Finance
-	// 	{
-	// 		Id = financeId,
-	// 		RepaymentSchedule = new RepaymentSchedule
-	// 		{
-	// 			FinanceId = financeId,
-	// 			Instalments = new List<Instalment>
-	// 						{
-	// 							new Instalment { PeriodEnd = today.AddDays(-10) },
-	// 							new Instalment { PeriodEnd = today, InstalmentInfo = new InstalmentInfo(), PaymentDue = 100},
-	// 							new Instalment { PeriodEnd = today.AddDays(10) }
-	// 						}
-	// 		}
-	// 	};
+		// And I have instantiated my manager
+		var interestManager = new InterestManager(moqManager.Object);
 
-	// 	var oldBalanceInfo = new BalanceInfo { FinanceId = financeId, ExpectedPrincipal = 0, PrincipalInterest = 0, Principal = 123, ArrearsInterest = 456, FeesInterestBearing = 789, FeesNonInterestBearing = 12, InterestOnFees = 34 };
-	// 	finance.BalanceInfo = oldBalanceInfo;
-	// 	var newBalanceInfo = new BalanceInfo { FinanceId = financeId, ExpectedPrincipal = 0, PrincipalInterest = 0, Principal = 123, ArrearsInterest = 456, FeesInterestBearing = 789, FeesNonInterestBearing = 12, InterestOnFees = 34 };
-	// 	yield return new object[] { financeId, today, finance, newBalanceInfo, 1, "No change expected" };
+		// When I call the service
+		var result = interestManager.CalculateInterest();
 
-	// }
+		// Then I expect the resulting interest amount to match the expected interest amount
+		Assert.AreEqual(Math.Round(expectedInterestAmount, 2), Math.Round(result, 2));
+	}
+
+	private static IEnumerable<object[]> GetCalculateInterestData()
+	{
+		var interestData = new Interest
+		{
+			principal = 20000,
+			interestRate = 12,
+			period = 2
+		};
+		var expectedInterestAmount = 5088.00;
+		yield return new object[] { interestData, expectedInterestAmount };
+
+		interestData = new Interest
+		{
+			principal = 15000,
+			interestRate = 10,
+			period = 2
+		};
+		expectedInterestAmount = 3150.00;
+		yield return new object[] { interestData, expectedInterestAmount };
+
+		interestData = new Interest
+		{
+			principal = 25000,
+			interestRate = 20,
+			period = 5
+		};
+		expectedInterestAmount = 37208.00;
+		yield return new object[] { interestData, expectedInterestAmount };
+
+	}
 }
